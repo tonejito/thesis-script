bf3ac0ac3b711db88f631627de977086d0cd9901
Andrés Hernández <andres@tonejito.org>
2014-03-29 00:12:52 -0600

    + Add and delete simpleSecurityObject in ruby
    SSHA Password is generated using the method posted at
    
    <https://github.com/ruby-ldap/ruby-net-ldap/blob/master/lib/net/ldap/password.rb>

--- bin/ldap.rb
+++ bin/ldap.rb
@@ -7,6 +7,9 @@
 require 'rubygems'
 require 'net/ldap'
 
+require 'digest/sha1'
+require 'base64'
+
 ldap = Net::LDAP.new \
 :host => "localhost",
 :port => 389,
@@ -23,8 +26,31 @@ else
   print "= T . T =","\n"
 end
 
-filter = Net::LDAP::Filter.eq("cn","m1-p1")
+filter = Net::LDAP::Filter.eq("cn","test")
 treebase = "dc=xnas,dc=local"
+
+# https://github.com/ruby-ldap/ruby-net-ldap/blob/master/lib/net/ldap/password.rb
+#pw = Net::LDAP::Password.generate(:ssha,"test")
+#print pw
+str = "test"
+srand; salt = (rand * 1000).to_i.to_s
+print salt
+attribute_value = '{SSHA}' + Base64.encode64(Digest::SHA1.digest(str + salt) + salt).chomp!
+print attribute_value
+
+dn = "cn=test,dc=xnas,dc=local"
+attrs =
+{
+  :objectclass => [ "top" , "organizationalRole" , "simpleSecurityObject" ] ,
+  :userpassword => attribute_value ,
+}
+
+ldap.delete :dn => dn
+p ldap.get_operation_result
+
+ldap.add( :dn => dn , :attributes => attrs)
+p ldap.get_operation_result
+
 
 ldap.search(:base => treebase , :filter => filter) do |entry|
   puts "DN: #{entry.dn}"
