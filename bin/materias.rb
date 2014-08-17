f9f95d719e3bbb1e73ed45e7433126ecd4ac1d1d
Andrés Hernández <andres@tonejito.org>
2014-08-17 01:02:58 -0500

    Refactoring in materias.rb

--- bin/materias.rb
+++ bin/materias.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 # encoding: utf-8
+# vim : filetype=ruby
 
 require 'csv'
 require 'rubygems'
@@ -8,7 +9,7 @@ require 'net/ldap'
 treebase = "dc=xnas,dc=local"
 
 ldap = Net::LDAP.new \
-:host => "localhost",
+:host => "thesis",
 :port => 389,
 :auth =>
 {
@@ -23,44 +24,58 @@ exit unless(ldap.bind)
 materias = []
 i=0
 
+def get_common_name(identifier="")
+  # Remove all accents and spaces from name to convert to cn
+  return identifier.gsub(/\ /,'-').gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'A','É'=>'E','Í'=>'I','Ó'=>'O','Ú'=>'U','Ü'=>'U','Ñ'=>'N').downcase
+end
+
+def get_display_name(identifier="")
+  # Convert name to "Sentence Case" including accents and special chars
+  return identifier.gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'á','É'=>'é','Í'=>'í','Ó'=>'ó','Ú'=>'ú','Ü'=>'ü','Ñ'=>'ñ').split(" ").map {|word| word.capitalize}.join(" ")
+end
+
 # Parse CSV file
 CSV.foreach("../materias.csv") do |row|
   # Get row contents
-  id_materia, materia = row
+   id_materia, materia = row
 
   # Skip header
   next if id_materia == "CLAVE"
 
   # Put row into array
   materias[i] = [ id_materia , materia ]
-  i+=1
+  i += 1
 
   # Remove all accents and spaces from name to convert to cn
-  cn = materia.gsub(/\ /,'-').gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'A','É'=>'E','Í'=>'I','Ó'=>'O','Ú'=>'U','Ü'=>'U','Ñ'=>'N').downcase
+  commonName = get_common_name(materia)
 
   # Get current DN
-  dn = "cn="+cn+","+"ou=materias"+","+treebase
+  displayName = "cn=" + commonName + "," + "ou=materias" + "," + treebase
 
-  # Search directory for DN whitout subtree and save as boolean
-  result = ldap.search(:base => dn , :scope => Net::LDAP::SearchScope_BaseObject , :return_result => false)
+  # Search directory for DN without subtree and save as boolean
+  result = ldap.search(:base => displayName , :scope => Net::LDAP::SearchScope_BaseObject , :return_result => false)
   
   # Convert name to "Sentence Case" including accents and special chars
-  materia = materia.gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'á','É'=>'é','Í'=>'í','Ó'=>'ó','Ú'=>'ú','Ü'=>'ü','Ñ'=>'ñ').split(" ").map {|word| word.capitalize}.join(" ")
+  materia = get_display_name(materia)
 
   # Insert object unless it already exists
   unless(result)
-    puts "+ "+dn
+    puts "+ " + displayName
     attrs =
     {
-      :objectclass => [ "top" , "organizationalRole"] ,
-      :cn => cn ,
+      :objectclass =>
+      [
+        "top" ,
+        "organizationalRole"
+      ] ,
+      :cn => commonName ,
       :description => materia ,
     }
     
     # Insert object in LDAP tree
-    ldap.add( :dn => dn , :attributes => attrs)
+    ldap.add( :dn => displayName , :attributes => attrs)
     # Search and print object as returned from the LDAP tree
-    ldap.search(:base => dn , :scope => Net::LDAP::SearchScope_BaseObject) do |entry|
+    ldap.search(:base => displayName , :scope => Net::LDAP::SearchScope_BaseObject) do |entry|
       puts "DN: #{entry.dn}"
       entry.each do |atribute , values|
         puts "	#{atribute}:"
@@ -72,7 +87,6 @@ CSV.foreach("../materias.csv") do |row|
 
   else
     # Object already exists (no change)
-    puts "= "+dn
+    puts "= "+displayName
   end
 end
-
