fa9352c70ad62f1f62d0713a5d2723784caf5889
Andrés Hernández <andres@tonejito.org>
2014-09-03 03:45:57 -0500

    Batch loading script - utility functions
    + String normalization functions
    + Added <grp> to groupOfNames CN (p-m)
    + Added feedback routine to print bad groups to CSV to fix manually and re-feed
    
    Recommended run:
    
    % ./clean.rb && ./csv.rb

--- bin/csv.rb
+++ bin/csv.rb
@@ -20,22 +20,22 @@ def get_name(identifier="")
 end
 
 def get_common_name(identifier="")
-  if (identifier.nil?)
-    puts "!!! identifier NIL" if (DEBUG)
-    return ""
-  end
   # Remove all accents and spaces from name to convert to cn
-  return identifier.gsub(/\ /,'-').gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'A','É'=>'E','Í'=>'I','Ó'=>'O','Ú'=>'U','Ü'=>'U','Ñ'=>'N').downcase
+  return strip_accents(identifier).gsub(/\ /,'-').downcase
 end
 
 def get_display_name(identifier="")
   # Convert name to "Sentence Case" including accents and special chars
-  return identifier.gsub(/[ÁÉÍÓÚÜÑ]/,'Á'=>'á','É'=>'é','Í'=>'í','Ó'=>'ó','Ú'=>'ú','Ü'=>'ü','Ñ'=>'ñ').split(" ").map {|word| word.capitalize}.join(" ")
+  return strip_accents(identifier).split(" ").map {|word| word.capitalize}.join(" ")
+end
+
+def get_ascii_string(string="")
+  return string.dup.force_encoding("ASCII-8BIT")
 end
 
 def print_result(ldap)
   if (DEBUG)
-    p ldap.get_operation_result unless (ldap.get_operation_result.code == 0)
+    p ldap.get_operation_result
   else
     p ldap.get_operation_result unless (ldap.get_operation_result.code == 0)
   end
@@ -47,6 +47,43 @@ def valid_group(p_name)
     return false if p_name == n
   end
   return true
+end
+
+# normalization functions
+
+def strip_accents(string="")
+  replacement =
+  {
+    'á' => 'a','à' => 'a','Á' => 'A','À' => 'A',
+    'é' => 'e','è' => 'e','È' => 'E','É' => 'E',
+    'í' => 'i','ì' => 'i','Ì' => 'I','Í' => 'I',
+    'ó' => 'o','ò' => 'o','Ò' => 'O','Ó' => 'O',
+    'ú' => 'u','ù' => 'u','Ù' => 'U','Ú' => 'U',
+    'ñ' => 'n','Ñ' => 'N','ü' => 'u','Ü' => 'U'
+  }
+  return string.gsub(/[áàÁÀéèÈÉíìÌÍóòÒÓúùÙÚñüÑÜ]/,replacement)
+end
+
+def strip_title_tag(string="")
+  return string.chomp.upcase.gsub(/\s+/,' ').gsub(/(M\.[ICAG]|L\.A|I\.Q|ING|FIS|MTRO|MRTO|DRA?)\.?$/,"")
+end
+
+def normalize(string)
+  # Remove <CR> or <CR><LF>
+  string = string.chomp
+  # Remove leading and trailing space characters and also compact white spaces
+  string = string.gsub(/(^\s+)|(\s+$)/,'').gsub(/\s+/,' ')
+  # strip punctuation signs
+  string = string.gsub(/[.,]/,'')
+  # Strip title tag
+  string = strip_title_tag(string)
+  # Convert spaces into dashes
+  string = string.gsub(/\ /,'-')
+  # Replace accented letters with [[:alpha:]] chars
+  string = strip_accents(string).downcase
+  # Remove characters not in the range of \w [a-zA-Z0-9_]
+  string = string.split("-").map {|word| word.gsub(/\W/,'')}.join("-")
+  return string
 end
 
 ########	vars
@@ -64,7 +101,7 @@ login_shell = "/usr/sbin/nologin"
 employee_type = "profesor"
 user_password = "thesis"
 gid_number = number
-mail = "nobody@localhost"
+fallback_mail = "nobody@localhost"
 
 ldap = Net::LDAP.new \
 :host => "thesis",
@@ -72,7 +109,7 @@ ldap = Net::LDAP.new \
 :auth =>
 {
  :method => :simple,
- :username => "cn=admin,dc=xnas,dc=local",
+ :username => "cn=admin"+","+treebase,
  :password => "thesis",
 }
 
@@ -84,13 +121,16 @@ exit unless(ldap.bind)
 profesores = []
 i=0
 
-CSV.foreach("../profesores2.csv") do |row|
-  rfc, name = row
+CSV.foreach("../profesores3.csv") do |row|
+#CSV.foreach("../profesores2.csv") do |row|
+#CSV.foreach("../profesores-fail.csv") do |row|
+  rfc, name, mail = row
   next if rfc == "RFC"
   next if rfc == "SIN"
-  name = get_name(name)
+  name = get_name(strip_accents(name))
   rfc = rfc[0,9]
-  profesores[i] = [rfc, name]
+  mail = fallback_mail if (mail.nil? or mail.empty?)
+  profesores[i] = [rfc, name, mail]
   i+=1
   
   common_name  = get_common_name(name)
@@ -109,7 +149,7 @@ CSV.foreach("../profesores2.csv") do |row|
       "shadowAccount"
     ] ,
     :uid            => rfc ,
-    :cn             => name ,
+    :cn             => normalize(name) ,
     :sn             => "." ,
     :mail           => mail ,
     :description    => displayName ,
@@ -186,6 +226,14 @@ CSV.foreach("../profesores2.csv") do |row|
   number+=1
 end
 
+#puts YAML::dump(profesores) if (DEBUG)
+
+#  profesores.each do |profesor|
+#    #puts profesor.inspect
+#    #puts YAML::dump(profesor)
+#    puts profesor[1]
+#  end
+
 ########	./materias.rb
 
 materias = []
@@ -195,8 +243,11 @@ current = last = nil
 
 # Parse CSV file
 CSV.foreach("../materias2.csv") do |row|
+#CSV.foreach("../materias-fail.csv") do |row|
   # Get row contents
-   id, grupo, materia, p_name = row
+  id, grupo, materia, p_rfc, p_name = row
+
+  p_rfc = "" if (p_rfc.nil? or p_rfc.empty?)
 
   #puts YAML::dump(row) if (DEBUG)
   
@@ -256,10 +307,10 @@ CSV.foreach("../materias2.csv") do |row|
 
       # Put row into array
       materias[i] = [ id, grupo, materia, rfc , p_name ]
-      i += 1
       #mat[:id] => { :name => materia , :group => grupo , :rfc => rfc }
       
-      group_of_names_cn = rfc + "-" + id
+      group_of_names_cn = rfc + "-" + id + "-" + grupo
+      #group_of_names_cn = materias[i][3] + "-" + materias[i][0] + "-" + materias[i][1]
       group_of_names_dn = "cn=" + group_of_names_cn + "," + "ou=webdav,ou=groups" + "," + treebase
       group_of_names_attributes = 
       {
@@ -269,12 +320,14 @@ CSV.foreach("../materias2.csv") do |row|
           "groupOfNames"
         ] ,
         :cn => group_of_names_cn ,
-        :description => p_name + " + " + materia ,
+        :description => [ rfc , "m" + id , "g" + grupo ] ,
         :owner => "uid="+rfc+",ou=profesores,ou=users,"+treebase,
         # when processing students we must add the "member" attribute via ldap.add
         :member => "uid="+rfc+",ou=profesores,ou=users,"+treebase,
+        # relationship wuth the subject
+        :seeAlso => displayName,
       }
-
+      i += 1
       #puts YAML::dump([group_of_names_dn , group_of_names_attributes]) if (DEBUG)
       ldap.add( :dn => group_of_names_dn , :attributes => group_of_names_attributes)
       print_result(ldap)
@@ -306,4 +359,43 @@ CSV.foreach("../materias2.csv") do |row|
   end
 end
 
-puts "	= ^ . ^ ="
+#puts YAML::dump(bad) if (DEBUG)
+def badish
+# Post process each bad entry to match against the profesor names in a regular expression fashion
+bad.each do |item|
+  # unpack
+  id      = materias[item][0]
+  rfc     = materias[item][3]
+  p_name  = materias[item][4]
+  #puts materias[item].inspect if (DEBUG)
+
+  #name = []
+  profesores.each do |profesor|
+    # normalize string and split into words
+    name = profesor[1].chomp.upcase.gsub(/\s+/,' ').gsub(/(M\.[ICAG]|L\.A|I\.Q|ING|FIS|MTRO|MRTO|DRA?)\.?$/,"").split(" ")
+    # match the known name against a regular expression
+    if (name.length >= 5)
+      regex = Regexp.new("^"+name[0]+" "+name[1]+" "+name[2]+" "+name[3]+" "+name[4])
+      puts [p_name , name , regex].inspect if (p_name =~ regex)
+    end
+    if (name.length >= 4)
+      regex = Regexp.new("^"+name[0]+" "+name[1]+" "+name[2]+" "+name[3])
+      puts [p_name , name , regex].inspect if (p_name =~ regex)
+    end
+    if (name.length >= 3)
+      regex = Regexp.new("^"+name[0]+" "+name[1]+" "+name[2])
+      puts [p_name , name , regex].inspect if (p_name =~ regex)
+    end
+    if (name.length >= 2)
+      regex = Regexp.new("^"+name[0]+" "+name[1])
+      puts [p_name , name , regex].inspect if (p_name =~ regex)
+    end
+  end
+end
+end
+
+puts "#	= ^ . ^ ="
+
+#materias.each do |materia|
+#  puts YAML::dump(materia) if (!materia[3].nil?)
+#end
